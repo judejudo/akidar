c3ee7456036a5aba47b50f7cf44368190156cf1e
judejudo <ocomilj@gmail.com>
2025-03-31 01:30:10 +0300

    layout3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Service from "./pages/Service";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 
 const queryClient = new QueryClient();
 
